474d7f1e514cfdf06b69750c94c6a765a29703e6
Mariusz Dabrowski <wayrse@gmail.com>
2026-05-14 16:12:16 -0400

    Match sidebar ticket pill styling to calendar pills

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1038,6 +1038,12 @@ section:not(.drawer-open):not(.drawer-closing) .section-header:hover {
   white-space: nowrap;
   overflow: visible;
   max-width: 100%;
+  text-shadow:
+    0 -1px 0 rgba(0, 0, 0, 0.3),
+    0 1px 0 rgba(255, 255, 255, 0.07);
+  box-shadow:
+    inset 0 1px 0 rgba(255, 255, 255, 0.1),
+    inset 0 -1px 0 rgba(0, 0, 0, 0.1);
 }
 
 .ticket-pill:active {
