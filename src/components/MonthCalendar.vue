7f0cbae819d335c4c0ffc382019262b70a1f6095
Mariusz Dabrowski <wayrse@gmail.com>
2026-05-14 12:05:57 -0400

    Match vacation pinstripe thickness to label pinstripe (3px/6px)

--- src/components/MonthCalendar.vue
+++ src/components/MonthCalendar.vue
@@ -559,7 +559,7 @@ function ticketSegmentBg(ticket: Ticket, spanIndex: number, spanTotal: number): 
 
 function vacationStyle(): Record<string, string> {
   return {
-    background: 'repeating-linear-gradient(45deg, #2e2e2e 0px, #2e2e2e 5px, #272727 5px, #272727 10px)',
+    background: 'repeating-linear-gradient(45deg, #2e2e2e 0px, #2e2e2e 3px, #272727 3px, #272727 9px)',
   }
 }
 
