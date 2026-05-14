c5ef6b7c264059bfe7fdd21afa99daec985155e1
Mariusz Dabrowski <wayrse@gmail.com>
2026-05-14 19:00:39 -0400

    Set day number color to rgba(255,255,255,0.4)

--- src/components/MonthCalendar.vue
+++ src/components/MonthCalendar.vue
@@ -938,7 +938,7 @@ h2 {
   flex-shrink: 0;
   font-size: 0.82rem;
   font-weight: 500;
-  color: rgba(255, 255, 255, 0.2);
+  color: rgba(255, 255, 255, 0.4);
   transition: background 0.1s, color 0.1s;
 }
 
