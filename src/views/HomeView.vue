c3647795d90e1fb3f9224891af4d26236fff1d2a
Mariusz Dabrowski <wayrse@gmail.com>
2026-05-15 18:05:00 -0400

    Fix Got it hover with all: unset + display: block to restore button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1094,7 +1094,7 @@ function onEventListDrop(event: DragEvent) {
       >
         <div class="hint-bubble hint-arrow-down">
           <span><svg class="hint-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M480-80q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-200v-80h320v80H320Zm10-120q-69-41-109.5-110T180-580q0-125 87.5-212.5T480-880q125 0 212.5 87.5T780-580q0 81-40.5 150T630-320H330Zm24-80h252q45-32 69.5-79T700-580q0-92-64-156t-156-64q-92 0-156 64t-64 156q0 54 24.5 101t69.5 79Zm126 0Z"/></svg>Interact with items by moving, scaling, and clicking into them</span>
-          <span class="hint-gotit" @click.stop="dismissHint(0)">Got it</span>
+          <button class="hint-gotit" @click.stop="dismissHint(0)">Got it</button>
         </div>
       </div>
       <div
@@ -1251,6 +1251,8 @@ function onEventListDrop(event: DragEvent) {
 }
 
 .hint-gotit {
+  all: unset;
+  display: block;
   align-self: flex-end;
   border-top: 1px solid rgba(255, 255, 255, 0.15);
   padding: 0.3rem 0 0;
